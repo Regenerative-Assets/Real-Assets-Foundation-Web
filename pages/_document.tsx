25ee008fb297f3710646db83120708db54f220a2
eternal8pcurr-lang <eternal8pcurr@gmail.com>
2025-11-20 21:14:05 +0000

    New Assets Profile Image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,12 +8,12 @@ export default function Document() {
         <meta name="description" content="Real Assets Foundation - Building regenerative asset systems" />
         <meta property="og:title" content="Real Assets Foundation" />
         <meta property="og:description" content="Building regenerative asset systems" />
-        <meta property="og:image" content="https://img.plasmic.app/img-optimizer/v1/img?src=https%3A%2F%2Fimg.plasmic.app%2Fimg-optimizer%2Fv1%2Fimg%2F38529cdf60b4cabe1d4a7022c89b6cd1.png&w=384&q=75&f=webp" />
+        <meta property="og:image" content="https://site-assets.plasmic.app/0221e58a767881fbabb8d07715678d6b.png" />
         <meta property="og:url" content="https://www.realassets.foundation" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Real Assets Foundation" />
         <meta name="twitter:description" content="Building regenerative asset systems" />
-        <meta name="twitter:image" content="https://img.plasmic.app/img-optimizer/v1/img?src=https%3A%2F%2Fimg.plasmic.app%2Fimg-optimizer%2Fv1%2Fimg%2F38529cdf60b4cabe1d4a7022c89b6cd1.png&w=384&q=75&f=webp" />
+        <meta name="twitter:image" content="https://site-assets.plasmic.app/0221e58a767881fbabb8d07715678d6b.png" />
       </Head>
       <body>
         <Main />
